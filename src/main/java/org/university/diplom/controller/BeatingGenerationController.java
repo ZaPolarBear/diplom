fe746733168e09ac9c2f733163192b02d633b859
ZaPolarBear <kalishuk2015@gmail.com>
2024-05-16 14:20:11 +0300

    addet test for service

--- src/main/java/org/university/diplom/controller/BeatingGenerationController.java
+++ src/main/java/org/university/diplom/controller/BeatingGenerationController.java
@@ -1,7 +1,13 @@
 package org.university.diplom.controller;
 
+import io.minio.errors.MinioException;
+import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +20,7 @@ import org.university.diplom.dto.ResultDto;
 import org.university.diplom.processor.Processor;
 import org.university.diplom.service.impl.MinioService;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @Controller
@@ -26,6 +33,9 @@ public class BeatingGenerationController {
     @Value("${spring.minio.bucket.image}")
     private String bucketImageName;
 
+    @Value("${spring.minio.bucket.file}")
+    private String bucketXmlName;
+
     @PostMapping("/beating")
     public String process(CommonDto commonDto, Model model) {
         commonDto.setType(FunctionType.BEATING);
@@ -37,5 +47,23 @@ public class BeatingGenerationController {
     @ResponseBody
     @GetMapping("/beating/image/{imageName}")
     public byte[] downloadImage(@PathVariable UUID imageName) {return minioService.find(imageName.toString(), bucketImageName);
+    }
+
+    @ResponseBody
+    @GetMapping("/beating/table/{tableName}")
+    public ResponseEntity<Resource> downloadTable(@PathVariable UUID tableName) throws IOException, MinioException {
+        String fileName = tableName.toString() + ".xml";
+
+        byte[] xmlBytes = minioService.find(fileName, bucketXmlName);
+
+        ByteArrayResource resource = new ByteArrayResource(xmlBytes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_XML)
+                .body((Resource) resource);
     }
 }
